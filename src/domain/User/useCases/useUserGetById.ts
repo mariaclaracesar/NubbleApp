5d66cf4cb0c19ecad18721921f3da43c66a64f0b
mariaclaracesar <mariaclaracesar1_5@hotmail.com>
2024-06-17 15:30:13 -0300

    feat: stale time

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -4,31 +4,11 @@ import {useQuery} from '@tanstack/react-query';
 import {userService} from '../userService';
 
 export function useUserGetById(id: number) {
-  // const [user, setUser] = useState<User>();
-  // const [error, setError] = useState<boolean | null>(null);
-  // const [loading, setLoading] = useState(false);
-
   const {data, isLoading, isError} = useQuery({
     queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => userService.getById(id),
+    staleTime: 1000 * 30,
   });
-
-  // // Essa função é responsável por chamar o método getById do userService para buscar um usuário pelo id fornecido.
-  // const getUserById = useCallback(async () => {
-  //   try {
-  //     setLoading(true);
-  //     const _user = await userService.getById(id);
-  //     setUser(_user);
-  //   } catch (er) {
-  //     setError(true);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }, [id]);
-
-  // useEffect(() => {
-  //   getUserById();
-  // }, [getUserById]);
 
   return {
     user: data,
